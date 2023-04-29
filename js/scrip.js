4c44f7dd054c701dc2743df461da6a0c846d47c3
gabsmelin <rm550202>
2023-04-28 21:00:06 -0300

    28-04

---
+++ js/scrip.js
@@ -0,0 +1,21 @@
+console.log("Olá mundo!");
+
+var nr1 = "7";
+var nr2 = "3";
+
+//Realizando operações
+console.log(parseInt(nr1)+parseInt(nr2));
+console.log("-------------------------")
+
+var nome1 = "guga";
+let nome2 = "igot";
+const nome3 = "lukinhas";
+
+console.log(nome3, nome2);
+if(nome1 != "") {
+    let nome1 = "Igot";
+}
+
+console.log(nome1);
+//diferença entre var e let
+
