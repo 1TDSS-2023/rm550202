3e45ac8c348963f72e7f61687aa42c87267ca6d7
gabsmelin <rm550202@fiap.com.br>
2023-05-12 22:43:03 -0300

    12/5

---
+++ js/calc.js
@@ -0,0 +1,29 @@
+//desafio 1 
+//inserir o value dos botoes de número e operações no visor
+//obrigatório ultilizar funções parametrizadas
+//Pode ultilizar enventos inline
+function insereNumeroNoVisor(botao, input) {
+    if(botao.value == "+" || botao.value == "-" || botao.value == "*" ||botao.value == "/") {
+        if(visor.value[visor.value.length-1] == "+" || visor.value[visor.value.length-1] == "-" || visor.value[visor.value.length-1] == "*" ||visor.value[visor.value.length-1] == "/") {
+            visor.value.slice(visor.value[visor.value.length-1])
+        } else {
+            input.value += botao.value;
+        }
+    } else {
+        input.value += botao.value;
+    }
+
+}
+
+
+//Desafio 2
+//arrow function(simplifica o código)
+const calculo = (visor) => {
+    let valor = visor.value;
+    visor.value = eval(valor);
+}
+//function
+function soma(visor) {
+    let valor = visor.value;
+    visor.value = eval(valor);
+}
